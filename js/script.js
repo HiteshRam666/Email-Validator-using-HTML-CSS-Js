f14314a6cea1ec79d81bbb3d7507c98910475ed5
Hitesh Ram <hiteshram321@gmail.com>
2023-09-19 22:35:56 +0530

    Files added

---
+++ js/script.js
@@ -0,0 +1,37 @@
+let result = {
+  tag: "",
+  free: true,
+  role: false,
+  user: "hiteshram321",
+  email: "hiteshram321@gmail.com",
+  score: 0.64,
+  state: "deliverable",
+  domain: "gmail.com",
+  reason: "valid_mailbox",
+  mx_found: true,
+  catch_all: null,
+  disposable: false,
+  smtp_check: true,
+  did_you_mean: "",
+  format_valid: true,
+};
+
+submitBtn.addEventListener("click", async (e) => {
+    e.preventDefault()
+    console.log("Clicked!")
+    resultCont.innerHTML = `<img width="123" src="img/loading.svg" alt="">`
+    let key = "ema_live_wWg0iMLxOIH1E24ojSwimJwITg3yotQBRPVWY1vs"
+    let email = document.getElementById("username").value 
+    let url = `https://api.emailvalidation.io/v1/info?apikey=${key}&email=${email}`
+    let res = await fetch(url)
+    let result = await res.json()
+    let str = ``
+    for (key of Object.keys(result)) {
+        if(result[key] !== "" && result[key]!== " "){ 
+            str = str + `<div>${key}: ${result[key]}</div>`
+        }
+    }
+
+    console.log(str)
+    resultCont.innerHTML = str
+})
